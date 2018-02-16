cb32d403ab58ab87338504ccf2998a37352c7ae6
Weston <wez470@gmail.com>
2018-02-15 23:15:02 -0600

    Add loggin.

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 const functions = require('firebase-functions');
 var axios = require("axios");
 
-var ALLOWED_DOMAINS = ["https://meta-chronic.com", "https://weed-exchange.firebaseapp.com"];
+var ALLOWED_DOMAINS = ["https://meta-chronic.com", "https://weed-exchange.firebaseapp.com", "us-central1-weed-exchange.cloudfunctions.net"];
 
 exports.searchStrain = functions.https.onRequest((request, response) => {
     var host = request.get('host');
-    console.log(host);
+    console.log(request);
+    console.log("Host: " + host);
     if (ALLOWED_DOMAINS.indexOf(host) >= 0) {
         response.setHeader('Access-Control-Allow-Origin', host);
     }
