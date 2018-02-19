ba25430aee00fa1522918462251837903db38971
Weston <wez470@gmail.com>
2018-02-19 15:02:29 -0600

    Update cloud function CORS.

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
 const functions = require('firebase-functions');
 var axios = require("axios");
 
-var ALLOWED_DOMAINS = ["https://meta-chronic.com", "https://weed-exchange.firebaseapp.com", "us-central1-weed-exchange.cloudfunctions.net"];
+var ALLOWED_DOMAINS = ["https://meta-chronic.com/", "https://weed-exchange.firebaseapp.com/"];
 
 exports.searchStrain = functions.https.onRequest((request, response) => {
-    var host = request.get('host');
-    console.log(request);
-    console.log("Host: " + host);
-    if (ALLOWED_DOMAINS.indexOf(host) >= 0) {
-        response.setHeader('Access-Control-Allow-Origin', host);
+    var ref = request.headers.referer;
+    console.log("Host: " + ref);
+    if (ALLOWED_DOMAINS.indexOf(ref) >= 0) {
+        response.setHeader('Access-Control-Allow-Origin', ref);
     }
     if (request.method === 'OPTIONS') {
         return;
